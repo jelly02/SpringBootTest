669d2b5be20dc144d3d0446fdc4e17c38dd91f46
JuriKim <40787487+jelly02@users.noreply.github.com>
2022-11-15 11:16:21 +0900

    Chore : 초기 컨트롤러 작성

--- myblog/src/main/java/com/myblog/myblog/controller/WelcomeClass.java
+++ myblog/src/main/java/com/myblog/myblog/controller/WelcomeClass.java
@@ -2,14 +2,22 @@ package com.myblog.myblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WelcomeClass {
 
-    //(1) GetMapping 작성하기
-    @GetMapping("/webblog")
+    
+    @GetMapping("/webblog") //(1) GetMapping 작성하기
+    @ResponseBody //(2) View 안 찾고 여기서 알아서 할게! -> 오류 안 남
     public String entryPoint(){
-        return "Hi";
+        return "Hi"; //(1-1) Hi 라는 템플릿이 없어서 오류 남
+    }
+
+    @GetMapping("/webblogpage")
+    @ResponseBody
+    public String viewPage(){
+        return "Index";
     }
 }
